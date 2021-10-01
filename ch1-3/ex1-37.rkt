8bb78c4df41a45818c0ef952d8c68ec8fe589fa0
KeisLuv5991 <chmhj9997@gmail.com>
2021-10-01 11:08:43 +0900

    [Fix] cont-frac 반복되는 프로시저 픽스

--- ch1-3/ex1-37.rkt
+++ ch1-3/ex1-37.rkt
@@ -11,10 +11,10 @@
 ; 반복 프로세스
 (define (cont-frac n d k)
   (define (f count result)
-    (if (> count k)
+    (if (< count 1)
         result
-        (f (+ count 1) (/ (n count) (+ (d count) result)))))
-  (f 0 0))
+        (f (- count 1) (/ (n count) (+ (d count) result)))))
+  (f k 0))
 
 
 ; 황금비는 1.618033988749895정도 된다. k값을 얼마나 주어야 1.6180까지 맞아떨어질까? 한번 찾아보자.
@@ -23,20 +23,20 @@
 (cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
            5)
-; 0.6153846153846154
+; 0.625
 
 (cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
-           7)
+           8)
 ; 0.6176470588235294
 
 (cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
-           9)
+           10)
 ; 0.6179775280898876
 
 (cont-frac (lambda (i) 1.0)
            (lambda (i) 1.0)
-           10)
+           11)
 ; 0.6180555555555556
 ; k가 10이 되면 소숫점 4자리까지 정확해진다.
--- ch1-3/ex1-38.rkt
+++ ch1-3/ex1-38.rkt
@@ -2,36 +2,19 @@
 
 (define (average a b c) (/ (+ a b c) 3.0))
 
-(define (cont-frac-iter n d k)
+(define (cont-frac n d k)
   (define (f count result)
-    (if (> count k)
+    (if (< count 1)
         result
-        (f (+ count 1) (/ (n count) (+ (d count) result)))))
-  (f 1 0))
-
-(define (cont-frac-recur n d k)
-  (define (f count)
-    (if (= count k)
-        (/ (n count) (d count))
-        (/ (n count) (+ (d count) (f (+ count 1))))))
-  (f 1))
+        (f (- count 1) (/ (n count) (+ (d count) result)))))
+  (f k 0))
 
 
-(+ 2 (cont-frac-recur (lambda (i) 1.0)
-                      (lambda (i)
-                        (if (= (remainder i 3) 2)
-                            (* 2 (/ (+ i 1) 3))
-                            1))
-                      30))
+(+ 2 (cont-frac (lambda (i) 1.0)
+                (lambda (i)
+                  (if (= (remainder i 3) 2)
+                      (* 2 (/ (+ i 1) 3))
+                      1))
+                30))
 ; 2.7182818284590455
-
-
-(+ 2 (cont-frac-iter (lambda (i) 1.0)
-                      (lambda (i)
-                        (if (= (remainder i 3) 2)
-                            (* 2 (/ (+ i 1) 3))
-                            1))
-                      30))
-; 2.953516797605659
-; 왜 반복 프로세스는 정확하지 않을까? 계산 방식은 바텀업 방식으로 정확히 동일한데;;;;
 
