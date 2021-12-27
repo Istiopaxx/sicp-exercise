83ab968d43769c428ce37ea6f1bee50f986580bb
KeisLuv5991 <chmhj9997@gmail.com>
2021-12-27 16:41:55 +0900

    K-181P. transform-painter를 이용한 flip-horiz,rotate180/270 정의하기

---
+++ ch2-2/ex2-50.rkt
@@ -0,0 +1,36 @@
+#lang sicp
+(#%require sicp-pict)
+
+
+; Exercise 2.50.
+
+; Define the transformation flip-horiz, which flips painters horizontally, and transformations that
+; rotate painters counterclockwise by 180 degrees and 270 degrees.
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180 painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270 painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint einstein)
+
+(paint (flip-horiz einstein))
+
+(paint (rotate-180 einstein))
+
+(paint (rotate-270 einstein))
+
+
