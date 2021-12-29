ea883cbf1e288d730d6c2c206b315ea355cd6982
KeisLuv5991 <chmhj9997@gmail.com>
2021-12-29 19:42:28 +0900

    K-187P. 두 글자데이터를 비교하는 equal? 프로시저

---
+++ ch2-3/ex2-54.rkt
@@ -0,0 +1,29 @@
+#lang sicp
+
+; Exercise 2.54.
+
+; Two lists are said to be equal? if they contain equal elements arranged in the same order.
+; For example,
+
+; (equal? '(this is a list) '(this is a list))
+
+; is true, but
+
+; (equal? '(this is a list) '(this (is a) list))
+
+; is false. 
+; To be more precise, we can define equal? recursively in terms of the basic eq? equality
+; of symbols by saying that a and b are equal? if they are both symbols and the symbols are eq?,
+; or if they are both lists such that (car a) is equal? to (car b) and (cdr a) is equal? to (cdr b).
+
+; Using this idea, implement equal? as a procedure.
+
+(define (equal? a b)
+  (if (not (and (pair? a) (pair? b)))
+      (and (equal? (car a) (car b))
+           (equal? (cdr a) (cdr b)))
+      (eq? a b)))
+
+(equal? '(this is a list) '(this is a list))
+
+(equal? '(this is a list) '(this (is a) list))
