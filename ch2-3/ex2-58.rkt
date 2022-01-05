b2adb1971b7608849e19b28080f98ac593ae0736
KeisLuv5991 <chmhj9997@gmail.com>
2022-01-05 20:00:37 +0900

    K-195P. infix 표기법 기호식의 미분

--- ch2-3/ex2-58.rkt
+++ ch2-3/ex2-58.rkt
@@ -74,58 +74,39 @@
 ;    is done before addition. Can you design appropriate predicates, selectors, and constructors
 ;    for this notation such that our derivative program still works?
 
+(define op-priority (list '* '+))
+(define (select-op s) (cadr s))
+(define (prior? op)
+  (if (equal? op (car op-priority))
+      #t
+      #f))
 
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else (list a1 '+ a2))))
+(define (next-op-exist? s) (and (not (null? (cdr s)))
+                                (not (null? (cddr s)))
+                                (not (null? (cdddr s)))
+                                (not (null? (cdr (cdddr s))))))
+(define (next-exp s) (cddr s))
 
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else (list m1 '* m2))))
-
-(define (sum? x)
-  (and (pair? x) (eq? (cadr x) '+)))
-
-(define (augend s) (car s))
-
-(define (addend s) (caddr s))
-
-(define (product? x)
-  (and (pair? x) (eq? (cadr x) '*)))
-
-(define (multiplicand p) (car p))
-
-(define (multiplier p) (caddr p))
-
-(define (deriv exp var)
+(define (deriv2 exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
          (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-         (make-sum (deriv (addend exp) var)
-                   (deriv (augend exp) var)))
-        ((product? exp)
-         (make-sum
-          (make-product (multiplier exp)
-                        (deriv (multiplicand exp) var))
-          (make-product (deriv (multiplier exp) var)
-                        (multiplicand exp))))
-        (else
-         (error "unknown expression type -- DERIV" exp))))
+        (else (let ((now-op (select-op exp)))
+                (cond ((prior? now-op)
+                       (make-sum
+                        (make-product (multiplier exp)
+                                      (deriv (multiplicand exp) var))
+                        (make-product (deriv (multiplier exp) var)
+                                      (multiplicand exp))))
+                      ((next-op-exist? exp)
+                       (deriv (make-sum (augend exp)
+                                        (next-exp exp))
+                              var))
+                      ((sum? exp)
+                       (make-sum (deriv (addend exp) var)
+                                 (deriv (augend exp) var)))
+                      (else (error "unknown expression type -- DERIV" exp)))))))
 
 
-
-
-
-
-
-
-
-
-
+(deriv2 '(x + 3 * (x + y + 2)) 'x)
 
