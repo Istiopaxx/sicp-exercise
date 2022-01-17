571f1052812b72dfc2ea88297422a732e59da9cc
KeisLuv5991 <chmhj9997@gmail.com>
2022-01-17 15:46:51 +0900

    K-260P. 같은 타입끼리의 coercion

---
+++ ch2-5/ex2-81.rkt
@@ -0,0 +1,315 @@
+#lang sicp
+
+
+
+; ==================================================
+; Define put/get, put/get-coercion, tag system, apply-generic.
+(define table (list))
+
+(define (put op type proc)
+  (set! table (append table (list (list op type proc)))))
+
+(define (get op type)
+  (define (search op type t)
+    (cond ((null? t) #f)  ; swallow error and get back #f, (error "unknown type in table -- GET")
+          ((and (equal? (caar t) op) (equal? (cadar t) type))
+           (caddar t))
+          (else (search op type (cdr t)))))
+  (search op type table))
+
+(define (put-coercion t1 t2 proc)
+  (set! table (append table (list (list t1 t2 proc)))))
+
+(define (get-coercion t1 t2)
+  (define (search t1 t2 table)
+    (cond ((null? table) (error "unknown type coercion in table -- GET-COERCION"))
+          ((and (equal? (caar table) t1) (equal? (cadar table) t2))
+           (caddar table))
+          (else (search t1 t2 (cdr table)))))
+  (search t1 t2 table))
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum -- CONTENTS" datum))))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types"
+                                (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+
+
+; ==================================================
+; Define scheme natural number system.
+
+(define (install-scheme-number-package)
+  (put 'add '(scheme-number scheme-number) (lambda (x y) (+ x y)))
+  (put 'sub '(scheme-number scheme-number) (lambda (x y) (- x y)))
+  (put 'mul '(scheme-number scheme-number) (lambda (x y) (* x y)))
+  (put 'div '(scheme-number scheme-number) (lambda (x y) (/ x y)))
+  (put 'equ? '(scheme-number scheme-number) (lambda (x y) (= x y)))
+  (put '=zero? '(scheme-number) (lambda (x) (= x 0)))
+  (put 'exp '(scheme-number scheme-number)
+       (lambda (x y) (expt x y))) ; using primitive expt
+  'done)
+
+(install-scheme-number-package)
+
+
+
+; ==================================================
+; Define rectangular and polar complex system.
+
+(define (square x) (* x x))
+
+(define (install-rectangular-package)
+  ;; internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-polar-package)
+  ;; internal procedures
+  (define (magnitude z) (car z))
+  (define (angle z) (cdr z))
+  (define (make-from-mag-ang r a) (cons r a))
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(install-polar-package)
+(install-rectangular-package)
+
+
+
+; ==================================================
+; Define complex number system.
+
+(define (install-complex-package)
+  ;; imported procedures from rectangular and polar packages
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  (define (real-part z)
+    (apply-generic 'real-part z))
+  (define (imag-part z)
+    (apply-generic 'imag-part z))
+  (define (magnitude z)
+    (apply-generic 'magnitude z))
+  (define (angle z)
+    (apply-generic 'angle z))
+  ;; internal procedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  (define (equ? z1 z2)
+    (and (= (real-part z1) (real-part z2))
+         (= (imag-part z1) (imag-part z2))))
+  (define (=zero? z)
+    (and (= (real-part z) 0)
+         (= (imag-part z) 0)))
+  ;; interface to rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'equ? '(complex complex) equ?)
+  (put '=zero? '(complex) =zero?)
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(install-complex-package)
+
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+
+
+; ==================================================
+; Coercion.
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+
+
+
+; ==================================================
+; Exercise 2.81.
+
+; Louis Reasoner has noticed that apply-generic may try to coerce the arguments to each other's type
+; even if they already have the same type. Therefore, he reasons, we need to put procedures in the
+; coercion table to "coerce" arguments of each type to their own type.
+
+; For example, in addition to the scheme-number->complex coercion shown above, he would do:
+
+(define (scheme-number->scheme-number n) n)
+(define (complex->complex z) z)
+(put-coercion 'scheme-number 'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+
+
+
+; ==================================================
+; a. With Louis's coercion procedures installed, what happens if apply-generic is called with
+;    two arguments of type scheme-number or two arguments of type complex for an operation
+;    that is not found in the table for those types? For example, assume that we've defined
+;    a generic exponentiation operation:
+
+(define (exp x y) (apply-generic 'exp x y))
+
+; and have put a procedure for exponentiation in the Scheme-number package
+; but not in any other package:
+
+;; following added to Scheme-number package
+; (put 'exp '(scheme-number scheme-number)
+;      (lambda (x y) (tag (expt x y)))) ; using primitive expt
+
+; What happens if we call exp with two complex numbers as arguments?
+
+(define z1 (make-complex-from-mag-ang 3 4))
+(define z2 (make-complex-from-real-imag 4 5))
+
+
+; Change to comment when executing.
+(exp z1 z2) 
+; Infinite loop...
+
+; exp operation is not defined with complex - complex, so apply-generic try to coerce them.
+; Because of Louis's coercion procedures, apply-generic coerce complex to complex itself's type and
+; infinitely do this with recursive apply-generic calls.
+
+
+
+; ==================================================
+; b. Is Louis correct that something had to be done about coercion with arguments of the same type,
+;    or does apply-generic work correctly as is?
+
+; When coercion with arguments of the same type occurs, it must be done something.
+; Because in state of as it is, the apply-generic for exp operation in two complex type arguments
+; generate a error of get-coercion, not in apply-generic itself.
+; There is no proper exp operation with two complex type arguments, and there is no proper coercion
+; operation from complex to scheme-number, the type which define exp operation.
+
+
+
+; ==================================================
+; c. Modify apply-generic so that it doesn't try coercion if the two arguments have the same type.
+
+(define (apply-generic2 op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (if (equal? type1 type2)
+                    (error "No method for these types"
+                           (list op type-tags))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else (error "No method for these types"
+                                         (list op type-tags)))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
+; Test.
+(define (exp2 x y) (apply-generic2 'exp x y))
+
+(exp2 z1 z2)
+; No method for these types (exp (complex complex))
+;   context...:
+;     body of "~\SicpExercise\ch2-5\ex2-81.rkt"
+
+
